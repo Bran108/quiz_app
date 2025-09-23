0c7eac027c618434e6020729524c7deb74592910
Brandon Sanchez <bsanch05@nyit.edu>
2025-09-23 12:55:52 -0400

    new files made

---
+++ lib/quiz.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget{
+  const Quiz({super.key});
+
+  State<Quiz> createState(){
+    return _QuizState();
+
+  }
+}
+class _QuizState extends State<Quiz>{
+var activeScreen = StartScreen(switchScreen);
+void switchScreen(){
+  setState(
+    (){
+      activeScreen = QuestionsScreen();
+    }
+  );
+}
+
+  Widget build(BuildContext context){
+  return MaterialApp(
+    home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ]
+            ),
+          ),
+          child: const StartScreen()
+        ),
+      ),
+    );
+
+  }
+}
