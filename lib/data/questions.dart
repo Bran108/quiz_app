5eea43a7a5fc4acdea9fff9a114d3af67a95dd34
Brandon Sanchez <bsanch05@nyit.edu>
2025-09-25 12:46:35 -0400

    adding first 2 questions

---
+++ lib/data/questions.dart
@@ -0,0 +1,20 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion("Which Girlfriend of Spider-Man did the Green Goblin Kill?",
+  [
+    "Mary Jane Watson",
+    "Gwen Stacy",
+    "Betty Brant",
+    "Felicia Hardy",
+  ],
+  ),
+  QuizQuestion("What Year Did Daredevil Debut in the Comics?",
+  [
+    "1961",
+    "1962",
+    "1963",
+    "1964",
+  ],
+  )
+];
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/widgets.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:flutter/material.dart';
 
-class QuestionsScreen extends StatefulWidget{
-  const QuestionsScreen ({super.key});
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key});
 
   @override
   State<QuestionsScreen> createState() {
-   return _QuestionsScreenState();
+    return _QuestionsScreenState();
   }
 }
-class _QuestionsScreenState extends State<QuestionsScreen>{
 
+class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text('Questions Sreen');
+    final currentQuestion = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(currentQuestion.text, style: TextStyle(color: Colors.white)),
+          const SizedBox(height: 30),
+          ...currentQuestion.answers.map((item) {
+            return AnswerButton(answerText: item, onTap: () {});
+          },),
+        ],
+      ),
+    );
   }
 }
