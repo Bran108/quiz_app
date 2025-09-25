3fae9a2acbfe949539bb1367ffbfe6b5151be08a
Brandon Sanchez <bsanch05@nyit.edu>
2025-09-25 14:44:20 -0400

    6 questions in

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -16,5 +16,37 @@ const questions = [
     "1963",
     "1964",
   ],
-  )
+  ),
+  QuizQuestion("Which of These Characters Have Become a Herald of Galactus?",
+  [
+    "Star-Lord",
+    "Captian Marvel",
+    "Silver Surfer",
+    "Adam Warlock",
+  ],
+  ),
+  QuizQuestion("Which of These Characters is Not a Version of The Flash?",
+  [
+    "Jay Garrick",
+    "Barry Allen",
+    "Guy Gardner",
+    "Wally West",
+  ],
+  ),
+  QuizQuestion("What is Peacemakers Real Name?",
+  [
+    "Christopher Smith",
+    "Christopher Cooper",
+    "Adrian Miller",
+    "Adrian Chase",
+  ],
+  ),
+  QuizQuestion("What was Jason Todd Put Into That Brought Him Back to Life?",
+  [
+    "Lazarus River",
+    "Lazarus Pit",
+    "Pit of Life",
+    "River of Life",
+  ],
+  ),
 ];
